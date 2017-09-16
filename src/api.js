0ab3e2491774a92271178b8790315eed19191cb4
Asa Ayers <Asa@AsaAyers.com>
2017-09-16 09:47:43 -0700

    Fixes #1 Use .gitignore when present

--- src/api.js
+++ src/api.js
@@ -4,9 +4,12 @@ import fs from 'fs'
 import promisify from 'es6-promisify'
 import scanFile from './scan-file.js'
 import type { Context, FileInfo } from './types'
+import { compile as giCompile } from 'gitignore-parser'
 
 const readdir = promisify(fs.readdir)
+const readFile = promisify(fs.readFile)
 const stat = promisify(fs.stat)
+const exists = (file) => stat(file).catch(() => false).then(Boolean)
 
 export const expandFile = (filename: string) => {
   const fullPath = path.resolve(process.cwd(), filename)
@@ -18,16 +21,21 @@ export const expandFile = (filename: string) => {
   return fullPath
 }
 
-async function buildContext (root: string, context: Context = {}) {
+type BuilderOptions = {
+  src: string,
+  accepts: (string) => boolean
+}
+
+async function buildContext ({ src, accepts }: BuilderOptions, context: Context = {}) {
   const directories = []
-  const filenames = await readdir(root)
+  const filenames = await readdir(src)
 
   await Promise.all(filenames.map(async (filename) => {
-    if (filename === 'node_modules') return
     if (filename === '.git') return
-    if (filename === 'test') return
+    const file = path.resolve(src, filename)
 
-    const file = path.resolve(root, filename)
+    if (!accepts(file)) return
+
     const stats = await stat(file)
 
     if (stats.isDirectory()) {
@@ -41,21 +49,32 @@ async function buildContext (root: string, context: Context = {}) {
     }
   }))
 
-  await Promise.all(directories.map(d => buildContext(d, context)))
+  await Promise.all(directories.map(d => buildContext({ src: d, accepts }, context)))
 
   return context
 }
 
-export async function scanFiles (root: string, files: Array<string>) {
-  root = expandFile(root) + '/'
-  const rootStats = await stat(root)
+export async function scanFiles (src: string, files: Array<string>) {
+  src = expandFile(src) + '/'
+  const rootStats = await stat(src)
   if (!rootStats.isDirectory()) {
-    throw new Error(`Root must be a directory: ${root}`)
+    throw new Error(`Root must be a directory: ${src}`)
+  }
+  const packageJson = path.join(process.cwd(), 'package.json')
+  if (!(await exists(packageJson))) {
+    throw new Error(`Please run this from your project src`)
+  }
+
+  let accepts = (path) => true
+  const gitignore = path.join(process.cwd(), '.gitignore')
+  if (await exists(gitignore)) {
+    const content = await readFile(gitignore)
+    accepts = giCompile(String(content)).accepts
   }
 
   let context
   try {
-    context = await buildContext(root)
+    context = await buildContext({ src, accepts })
   } catch (e) {
     console.log('error building context: ', e)
     throw e
